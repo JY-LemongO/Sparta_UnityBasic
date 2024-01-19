c0dc739f90010720e302618285f40e8c947c0a7a
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-01-19 22:25:41 +0900

    Feat: Smooth Camera 구현
    Chore: 타일맵 수정

--- Assets/Scripts/Managers/Contents/CameraController.cs
+++ Assets/Scripts/Managers/Contents/CameraController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Player _playerController;
-
+    private Transform _playerTransform;
+    [SerializeField] Vector2 _borderLeftDown;
+    [SerializeField] Vector2 _borderRightUp;
+    [SerializeField] float _cameraSpeed;
     private void Start()
-    {        
-        _playerController = GameManager.Player;
+    {
+        _playerTransform = GameManager.Player.transform;
     }
 
-    private void LateUpdate()
-    {
-        transform.position = _playerController.transform.position - Vector3.forward * 10;
+    private void FixedUpdate()
+    {        
+        transform.position = Vector3.Lerp(transform.position, _playerTransform.position + Vector3.forward * -10, Time.deltaTime * _cameraSpeed);
+
+        float limitX = Mathf.Clamp(transform.position.x, _borderLeftDown.x, _borderRightUp.x);
+        float limitY = Mathf.Clamp(transform.position.y, _borderLeftDown.y, _borderRightUp.y);
+
+        transform.position = new Vector3(limitX, limitY, -10);
     }
 }
